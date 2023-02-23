269a9dca160148aba6f848707db9e7f3e471adc5
Chris <hakizachrist1@gmail.com>
2023-02-23 16:17:00 +0200

    Update calculator.js

--- src/Components/calculator.js
+++ src/Components/calculator.js
@@ -1,32 +1,45 @@
 import React from 'react';
 import './calculator.css';
 import Result from './result';
+import calculate from './logic/calculate';
 
 function Calculator() {
+  const [state, setState] = useState({ total: 0, next: '', operation: '' });
+  const { total, next, operation } = state;
+
+  const handlerClick = (e) => {
+    const updateState = calculate(state, e.target.name);
+    setState(updateState);
+  };
+
   return (
     <>
       <div className="container">
-        <Result />
+        <Result
+          total={total}
+          next={next}
+          operation={operation}
+        />
         <div className="keypad">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button type="button" className="highlight">÷</button>
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button type="button" className="highlight">*</button>
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button type="button" className="highlight">-</button>
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button type="button" className="highlight">+</button>
-          <button type="button" id="zero">0</button>
-          <button type="button">.</button>
-          <button type="button" className="highlight">=</button>
+          <button name="AC" type="button" onClick={handlerClick}>AC</button>
+          <button name="+/-" type="button" onClick={handlerClick}>+/-</button>
+          <button name="%" type="button" onClick={handlerClick}>%</button>
+          <button name="÷" type="button" onClick={handlerClick} className="highlight">÷</button>
+          <button name="7" type="button" onClick={handlerClick}>7</button>
+          <button name="8" type="button" onClick={handlerClick}>8</button>
+          <button name="9" type="button" onClick={handlerClick}>9</button>
+          <button name="*" type="button" onClick={handlerClick} className="highlight">*</button>
+          <button name="4" type="button" onClick={handlerClick}>4</button>
+          <button name="5" type="button" onClick={handlerClick}>5</button>
+          <button name="6" type="button" onClick={handlerClick}>6</button>
+          <button name="-" type="button" onClick={handlerClick} className="highlight">-</button>
+          <button name="1" type="button" onClick={handlerClick}>1</button>
+          <button name="2" type="button" onClick={handlerClick}>2</button>
+          <button name="3" type="button" onClick={handlerClick}>3</button>
+          <button name="+" type="button" onClick={handlerClick} className="highlight">+</button>
+          <button name="0" type="button" onClick={handlerClick} id="zero">0</button>
+          <button name="." type="button" onClick={handlerClick}>.</button>
+          <button name="=" type="button" onClick={handlerClick} className="highlight">=</button>
         </div>
       </div>
     </>
