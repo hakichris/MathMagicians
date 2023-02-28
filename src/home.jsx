4177e6474d11018e8626e45c3fcc145680660b6d
Chris <hakizachrist1@gmail.com>
2023-03-01 01:08:49 +0200

    Update home.jsx

---
+++ src/home.jsx
@@ -0,0 +1,12 @@
+const Home = () => (
+
+  <div className="home">
+    <div>
+      <h1 className="Title"> Math Magicians</h1>
+    </div>
+    <h2>Welcome to our page!</h2>
+    <p>i am gonna make this website works</p>
+  </div>
+);
+
+export default Home;
