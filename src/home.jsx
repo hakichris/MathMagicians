a1c9bf504167688f0ca7e11ca1d10aa6908d81b1
Chris <hakizachrist1@gmail.com>
2023-03-01 10:42:14 +0200

    Update home.jsx

--- src/home.jsx
+++ src/home.jsx
@@ -5,7 +5,17 @@ const Home = () => (
       <h1 className="Title"> Math Magicians</h1>
     </div>
     <h2>Welcome to our page!</h2>
-    <p>i am gonna make this website works</p>
+    <p>
+      Welcome to Math Magicians. Math magician is an online web app
+      that gives you access to Mathematical tools such as Calculator.
+      In addition, you get access to quotes from famous mathematicians.
+    </p>
+    <p>
+      The Calculator will allow you to do your math calculation easily and faster.
+      Besides, you don&apos;t need to spend money buying a physical calculator because
+      the Math Magician is equippedwith an edge-cut calculator that can solve most of
+      your calculations.
+    </p>
   </div>
 );
 
